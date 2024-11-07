399816328f4e03670657b7c6cc36deadb8453c8e
Christopher Moyano <cmoyano@ing.ucsc.cl>
2024-11-07 12:44:59 -0300

    termino del codigo

--- VendedorViajero.py
+++ VendedorViajero.py
@@ -38,7 +38,7 @@ def CrearColonia(col,n):
         Matriz[i][0] = np.random.randint(1,n+1)
     return Matriz
 
-def Transicion_1(Feromona, Heuristica,B,hormigas):
+def Transicion_1(Feromona, Heuristica,B,hormigas,q0,Feromona_inicial,tev):
     mascara = np.ones((hormigas.shape[0],hormigas.shape[1]),int)
     for i in range(hormigas.shape[0]):
         for j in range(hormigas.shape[1]):
@@ -46,14 +46,35 @@ def Transicion_1(Feromona, Heuristica,B,hormigas):
                 mascara[i][hormigas[i][j]-1] = 0  
     for i in range(hormigas.shape[0]):
         for j in range(hormigas.shape[1]-1):
-            vector_aux = np.power(Heuristica[hormigas[i][j]-1],B)*Feromona[hormigas[i][j]-1]*mascara[i]
-            pos_max = np.random.choice(np.where(vector_aux == np.max(vector_aux))[0])   #si me tira error en esta linea borrar un [0]
-            hormigas[i][j+1] = pos_max + 1
-            mascara[i][pos_max]=0 
+            random = np.random.rand()
+            if (random <= q0):
+                vector_aux = np.power(Heuristica[hormigas[i][j]-1],B)*Feromona[hormigas[i][j]-1]*mascara[i]
+                pos_max = np.random.choice(np.where(vector_aux == np.max(vector_aux))[0])   #si me tira error en esta linea borrar un [0]
+                hormigas[i][j+1] = pos_max + 1
+                mascara[i][pos_max] = 0
+            else:
+                nodo = Transicion_2(Feromona,Heuristica,B,hormigas,mascara,i,j)
+                hormigas[i][j+1] = nodo + 1
+                mascara[i][nodo] = 0
+            Actualizar_feromona_local(Feromona,tev,Feromona_inicial,hormigas[i][j]-1,hormigas[i][j+1]-1)
     return hormigas
 
 
 #implementar la ruleta
+def Transicion_2(Feromona,Heuristica,B,hormigas,mascara,i,j):
+    suma_total= np.sum(np.power(Heuristica[hormigas[i][j]-1],B)*Feromona[hormigas[i][j]-1]*mascara[i])
+    ruleta = (np.power(Heuristica[hormigas[i][j]-1],B)*Feromona[hormigas[i][j]-1]*mascara[i])/suma_total
+    for k in range(1,ruleta.shape[0]):
+        ruleta[k]=ruleta[k]+ruleta[k-1]
+    random = np.random.rand()
+    for k in range(ruleta.shape[0]):
+        if random < ruleta[k]:
+            return k
+
+def Actualizar_feromona_local(Feromona,tev,Feromona_inicial,hormiga,nodo):
+    Feromona[hormiga][nodo] = Feromona[hormiga][nodo]*(1-tev) + tev *Feromona_inicial[hormiga][nodo]
+    return Feromona
+
     
 if len(sys.argv)==8:
     semilla = int(sys.argv[1])
@@ -73,12 +94,29 @@ np.random.seed(semilla)
 matrizCoordenadas = LimpiezaDatos(entrada)
 matrizDistancia = distance_matrix(matrizCoordenadas,matrizCoordenadas)
 matrizHeuristica = CrearMatrizHeuristica(matrizDistancia)
-primeraSolucion = CrearPrimeraSolucion(matrizHeuristica.shape[0])
-Costo = CrearCosto(primeraSolucion,matrizDistancia)
-print(Costo)
-MatrizFeromonaInicial = CrearMatrizFeromonaInicial(Costo,matrizHeuristica.shape[0])
+Mejor_Solucion = CrearPrimeraSolucion(matrizHeuristica.shape[0])
+Mejor_Costo = CrearCosto(Mejor_Solucion,matrizDistancia)
+print(Mejor_Costo)
+MatrizFeromonaInicial = CrearMatrizFeromonaInicial(Mejor_Costo,matrizHeuristica.shape[0])
+Feromona = MatrizFeromonaInicial
 Colonia = CrearColonia(col,matrizHeuristica.shape[0])
-hormigas = Transicion_1(MatrizFeromonaInicial,matrizHeuristica,B,Colonia)
+iteracion =0
+while(iteracion<ite):
+    hormigas = Transicion_1(Feromona,matrizHeuristica,B,Colonia,q0,MatrizFeromonaInicial,tev)
+    for i in range(col):
+        NuevoCosto = CrearCosto(hormigas[i],matrizDistancia)
+        if NuevoCosto < Mejor_Costo:
+            Mejor_Costo = NuevoCosto   ##supuesto mejor costo 7542
+            Mejor_Solucion = hormigas[i]
+    Feromona = (1-tev)*Feromona
+    delta = 1/Mejor_Costo
+    for i in range(Mejor_Solucion.shape[0]-1):
+        Feromona[Mejor_Solucion[i]-1,Mejor_Solucion[i+1]-1]+= delta
+        Feromona[Mejor_Solucion[i+1]-1,Mejor_Solucion[i]-1] += delta
+    Feromona[Mejor_Solucion[0]-1,Mejor_Solucion[Mejor_Solucion.shape[0]-1]-1] +=delta
+    Feromona[Mejor_Solucion[Mejor_Solucion.shape[0]-1]-1,Mejor_Solucion[0]-1] +=delta
+    print(Mejor_Costo)
+    iteracion+=1
 
 
 
